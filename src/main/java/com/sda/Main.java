0a2e578d499c19ff30d2a9abc0976cd58e234629
Krzysztof Aniołkowski <kjaniol94@gmail.com>
2019-03-02 13:52:51 +0100

    Zmieniono wartosc

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -3,7 +3,7 @@ package com.sda;
 public class Main {
     public static void main(String[] args) {
         System.out.println("witaj w drużynie");
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 200; i++) {
             System.out.println("Coś tam");
 
         }
