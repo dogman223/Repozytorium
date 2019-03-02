2d74e49385dd9a408396e57df248a3ebff7b82cf
Krzysztof Aniołkowski <kjaniol94@gmail.com>
2019-03-02 14:04:05 +0100

    zmiana

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -2,7 +2,7 @@ package com.sda;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("witaj w drużynie");
+        System.out.println("Dzień dobry");
 
         for (int i = 0; i < 10; i++) {
 
